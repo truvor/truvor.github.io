5b485d74641f37084855b02f2a4adfd6400874f0
truvor <truvor187@gmai.com>
2023-04-30 17:45:41 -0400

    Add closing of the burger on click outside the menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'risky-landing';
+
+  ngOnInit() {
+    // Makes the burger close on click outside the menu
+    const menu = document.getElementsByClassName('top-nav')[0];
+    document.addEventListener('click', function(event) {
+      if (!menu.contains(event.target as HTMLElement)) {
+        const burger = document.getElementById('menu-toggle') as HTMLInputElement;
+        burger.checked = false;
+      }
+    });
+  }
+
+  closeBurgerMenu = () => {
+    const burger = document.getElementById('menu-toggle') as HTMLInputElement;
+    if (burger.checked) {
+      burger.checked = false;
+    }
+  }
 }
