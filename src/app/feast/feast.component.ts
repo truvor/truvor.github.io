7306bfa41f3433c4a96fc91dd993503139be56aa
truvor <truvor187@gmai.com>
2024-12-20 00:08:11 -0500

    Updated the links

--- src/app/feast/feast.component.ts
+++ src/app/feast/feast.component.ts
@@ -9,8 +9,7 @@ export class FeastComponent {
   linksList = [
     {name: 'Spotify', url: 'https://open.spotify.com/track/6IJjkXQHCdc1ghkWeoIT3I'},
     {name: 'Apple', url: 'https://music.apple.com/ae/album/feast/1782193085'},
-    {name: 'Pandora', url: 'https://pandora.app.link/PGCogOktfhb'},
-    {name: 'Deezer', url: 'https://deezer.page.link/AaKTRi4rpWxSdnuc8'},
+    {name: 'Deezer', url: 'https://deezer.page.link/7EnTJgT55wjQmM9t5'},
     {name: 'YT.Music', url: 'https://music.youtube.com/watch?v=BC4Ku0pZbDk'},
     {name: 'Soundcloud', url: 'https://soundcloud.com/risky4real/feast'},
     {name: 'Bandcamp', url: 'https://risky4real.bandcamp.com/track/feast'},
