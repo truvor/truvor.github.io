51fbb94ae8e94a840013ad4eaba094d9dec68778
truvor <truvor187@gmail.com>
2026-01-07 18:13:16 -0500

    Upgrade Angular to v21

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,22 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {
+  enableProdMode,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from "@angular/core";
 
-
-import { environment } from './environments/environment';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
-import { AppRoutingModule } from './app/app-routing.module';
-import { NgOptimizedImage } from '@angular/common';
-import { AppComponent } from './app/app.component';
+import { environment } from "./environments/environment";
+import { BrowserModule, bootstrapApplication } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app/app-routing.module";
+import { NgOptimizedImage } from "@angular/common";
+import { AppComponent } from "./app/app.component";
 
 if (environment.production) {
   enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule, AppRoutingModule, NgOptimizedImage)]
-})
-  .catch(err => console.error(err));
+  providers: [
+    provideZoneChangeDetection(),
+    importProvidersFrom(BrowserModule, AppRoutingModule, NgOptimizedImage),
+  ],
+}).catch((err) => console.error(err));
