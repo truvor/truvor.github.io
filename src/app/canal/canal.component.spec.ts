75f7b4272578293bb367aa1c79d2ffb138a04cc6
truvor <truvor187@gmail.com>
2026-01-07 15:42:20 -0500

    Fix tests

--- src/app/canal/canal.component.spec.ts
+++ src/app/canal/canal.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { CanalComponent } from './canal.component';
+import { CanalComponent } from "./canal.component";
+import { ActivatedRoute } from "@angular/router";
 
-describe('CanalComponent', () => {
+describe("CanalComponent", () => {
   let component: CanalComponent;
   let fixture: ComponentFixture<CanalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [CanalComponent]
-})
-    .compileComponents();
+      imports: [CanalComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { params: {}, url: [] } },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CanalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/trinity/trinity.component.spec.ts
+++ src/app/trinity/trinity.component.spec.ts
@@ -1,23 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { TrinityComponent } from './trinity.component';
+import { TrinityComponent } from "./trinity.component";
+import { ActivatedRoute } from "@angular/router";
 
-describe('TrinityComponent', () => {
+describe("TrinityComponent", () => {
   let component: TrinityComponent;
   let fixture: ComponentFixture<TrinityComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [TrinityComponent]
-})
-    .compileComponents();
+      imports: [TrinityComponent],
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { params: {}, url: [] } },
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TrinityComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
